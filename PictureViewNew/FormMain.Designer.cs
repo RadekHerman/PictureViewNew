7fa61a4420923565fec3b7ba5d6e1b8d39647383
RadekHerman <107422011+RadekHerman@users.noreply.github.com>
2024-12-09 23:02:59 +0100

    feat: menu layout added (not working idea)

--- PictureViewNew/FormMain.Designer.cs
+++ PictureViewNew/FormMain.Designer.cs
@@ -40,8 +40,24 @@
             saveAsButton = new Button();
             openFileDialog1 = new OpenFileDialog();
             saveFileDialog1 = new SaveFileDialog();
+            menuStrip1 = new MenuStrip();
+            imageToolStripMenuItem = new ToolStripMenuItem();
+            openToolStripMenuItem = new ToolStripMenuItem();
+            closeToolStripMenuItem = new ToolStripMenuItem();
+            exitToolStripMenuItem = new ToolStripMenuItem();
+            viewToolStripMenuItem = new ToolStripMenuItem();
+            fullScreenToolStripMenuItem = new ToolStripMenuItem();
+            rotateToolStripMenuItem = new ToolStripMenuItem();
+            resizeToolStripMenuItem = new ToolStripMenuItem();
+            propertiesToolStripMenuItem = new ToolStripMenuItem();
+            backgroundColorToolStripMenuItem = new ToolStripMenuItem();
+            resizeSettingsToolStripMenuItem = new ToolStripMenuItem();
+            aboutToolStripMenuItem = new ToolStripMenuItem();
+            nextImageToolStripMenuItem = new ToolStripMenuItem();
+            previousImageToolStripMenuItem = new ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             tableLayoutPanel1.SuspendLayout();
+            menuStrip1.SuspendLayout();
             SuspendLayout();
             // 
             // pictureBox1
@@ -50,9 +66,10 @@
             pictureBox1.BorderStyle = BorderStyle.Fixed3D;
             pictureBox1.Cursor = Cursors.Hand;
             pictureBox1.Dock = DockStyle.Fill;
-            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Location = new Point(0, 28);
+            pictureBox1.Margin = new Padding(3, 4, 3, 4);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(604, 371);
+            pictureBox1.Size = new Size(709, 470);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
@@ -79,13 +96,14 @@
             tableLayoutPanel1.Controls.Add(saveAsButton, 2, 0);
             tableLayoutPanel1.Cursor = Cursors.Hand;
             tableLayoutPanel1.Dock = DockStyle.Bottom;
-            tableLayoutPanel1.Location = new Point(0, 330);
+            tableLayoutPanel1.Location = new Point(0, 443);
+            tableLayoutPanel1.Margin = new Padding(3, 4, 3, 4);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
-            tableLayoutPanel1.Padding = new Padding(10, 5, 10, 5);
+            tableLayoutPanel1.Padding = new Padding(11, 7, 11, 7);
             tableLayoutPanel1.RightToLeft = RightToLeft.Yes;
             tableLayoutPanel1.RowCount = 1;
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
-            tableLayoutPanel1.Size = new Size(604, 41);
+            tableLayoutPanel1.Size = new Size(709, 55);
             tableLayoutPanel1.TabIndex = 1;
             tableLayoutPanel1.Paint += tableLayoutPanel1_Paint;
             // 
@@ -93,9 +111,10 @@
             // 
             closeButton.AutoSize = true;
             closeButton.AutoSizeMode = AutoSizeMode.GrowAndShrink;
-            closeButton.Location = new Point(509, 8);
+            closeButton.Location = new Point(594, 11);
+            closeButton.Margin = new Padding(3, 4, 3, 4);
             closeButton.Name = "closeButton";
-            closeButton.Size = new Size(82, 25);
+            closeButton.Size = new Size(101, 30);
             closeButton.TabIndex = 7;
             closeButton.Text = "Close Image";
             closeButton.UseVisualStyleBackColor = true;
@@ -105,9 +124,10 @@
             // 
             previousButton.AutoSize = true;
             previousButton.AutoSizeMode = AutoSizeMode.GrowAndShrink;
-            previousButton.Location = new Point(102, 8);
+            previousButton.Location = new Point(120, 11);
+            previousButton.Margin = new Padding(3, 4, 3, 4);
             previousButton.Name = "previousButton";
-            previousButton.Size = new Size(62, 25);
+            previousButton.Size = new Size(74, 30);
             previousButton.TabIndex = 1;
             previousButton.Text = "Previous";
             previousButton.UseVisualStyleBackColor = true;
@@ -117,9 +137,10 @@
             // 
             openButton.AutoSize = true;
             openButton.AutoSizeMode = AutoSizeMode.GrowAndShrink;
-            openButton.Location = new Point(14, 8);
+            openButton.Location = new Point(13, 11);
+            openButton.Margin = new Padding(3, 4, 3, 4);
             openButton.Name = "openButton";
-            openButton.Size = new Size(82, 25);
+            openButton.Size = new Size(101, 30);
             openButton.TabIndex = 0;
             openButton.Text = "Open Image";
             openButton.UseVisualStyleBackColor = true;
@@ -128,9 +149,10 @@
             // nextButton
             // 
             nextButton.AutoSizeMode = AutoSizeMode.GrowAndShrink;
-            nextButton.Location = new Point(170, 8);
+            nextButton.Location = new Point(200, 11);
+            nextButton.Margin = new Padding(3, 4, 3, 4);
             nextButton.Name = "nextButton";
-            nextButton.Size = new Size(62, 23);
+            nextButton.Size = new Size(71, 31);
             nextButton.TabIndex = 2;
             nextButton.Text = "Next";
             nextButton.UseVisualStyleBackColor = true;
@@ -139,9 +161,10 @@
             // rotateButton
             // 
             rotateButton.AutoSize = true;
-            rotateButton.Location = new Point(238, 8);
+            rotateButton.Location = new Point(277, 11);
+            rotateButton.Margin = new Padding(3, 4, 3, 4);
             rotateButton.Name = "rotateButton";
-            rotateButton.Size = new Size(63, 25);
+            rotateButton.Size = new Size(72, 33);
             rotateButton.TabIndex = 3;
             rotateButton.Text = "Rotate";
             rotateButton.UseVisualStyleBackColor = true;
@@ -151,9 +174,10 @@
             // 
             fullScreenButton.AutoSize = true;
             fullScreenButton.AutoSizeMode = AutoSizeMode.GrowAndShrink;
-            fullScreenButton.Location = new Point(429, 8);
+            fullScreenButton.Location = new Point(498, 11);
+            fullScreenButton.Margin = new Padding(3, 4, 3, 4);
             fullScreenButton.Name = "fullScreenButton";
-            fullScreenButton.Size = new Size(74, 25);
+            fullScreenButton.Size = new Size(90, 30);
             fullScreenButton.TabIndex = 6;
             fullScreenButton.Text = "Full Screen";
             fullScreenButton.UseVisualStyleBackColor = true;
@@ -162,9 +186,10 @@
             // saveButton
             // 
             saveButton.AutoSize = true;
-            saveButton.Location = new Point(307, 8);
+            saveButton.Location = new Point(355, 11);
+            saveButton.Margin = new Padding(3, 4, 3, 4);
             saveButton.Name = "saveButton";
-            saveButton.Size = new Size(55, 25);
+            saveButton.Size = new Size(63, 33);
             saveButton.TabIndex = 4;
             saveButton.Text = "Save";
             saveButton.UseVisualStyleBackColor = true;
@@ -172,9 +197,11 @@
             // 
             // saveAsButton
             // 
-            saveAsButton.Location = new Point(368, 8);
+            saveAsButton.AutoSize = true;
+            saveAsButton.Location = new Point(424, 11);
+            saveAsButton.Margin = new Padding(3, 4, 3, 4);
             saveAsButton.Name = "saveAsButton";
-            saveAsButton.Size = new Size(55, 23);
+            saveAsButton.Size = new Size(68, 31);
             saveAsButton.TabIndex = 5;
             saveAsButton.Text = "Save as";
             saveAsButton.UseVisualStyleBackColor = true;
@@ -191,24 +218,127 @@
             saveFileDialog1.Filter = "JPEG Files (*.jpg)|*.jpg|PNG Files (*.png)|*.png|BMP Files (*.bmp)|*.bmp|All files (*.*)|*.*";
             saveFileDialog1.FilterIndex = 0;
             // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { imageToolStripMenuItem, viewToolStripMenuItem, propertiesToolStripMenuItem, aboutToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(709, 28);
+            menuStrip1.TabIndex = 2;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // imageToolStripMenuItem
+            // 
+            imageToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { openToolStripMenuItem, closeToolStripMenuItem, exitToolStripMenuItem });
+            imageToolStripMenuItem.Name = "imageToolStripMenuItem";
+            imageToolStripMenuItem.Size = new Size(65, 24);
+            imageToolStripMenuItem.Text = "&Image";
+            // 
+            // openToolStripMenuItem
+            // 
+            openToolStripMenuItem.Name = "openToolStripMenuItem";
+            openToolStripMenuItem.Size = new Size(224, 26);
+            openToolStripMenuItem.Text = "&Open Image";
+            // 
+            // closeToolStripMenuItem
+            // 
+            closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            closeToolStripMenuItem.Size = new Size(224, 26);
+            closeToolStripMenuItem.Text = "&Close Image";
+            // 
+            // exitToolStripMenuItem
+            // 
+            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            exitToolStripMenuItem.Size = new Size(224, 26);
+            exitToolStripMenuItem.Text = "&Exit App";
+            // 
+            // viewToolStripMenuItem
+            // 
+            viewToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { fullScreenToolStripMenuItem, rotateToolStripMenuItem, resizeToolStripMenuItem, nextImageToolStripMenuItem, previousImageToolStripMenuItem });
+            viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            viewToolStripMenuItem.Size = new Size(55, 24);
+            viewToolStripMenuItem.Text = "&View";
+            // 
+            // fullScreenToolStripMenuItem
+            // 
+            fullScreenToolStripMenuItem.Name = "fullScreenToolStripMenuItem";
+            fullScreenToolStripMenuItem.Size = new Size(224, 26);
+            fullScreenToolStripMenuItem.Text = "&Full Screen";
+            // 
+            // rotateToolStripMenuItem
+            // 
+            rotateToolStripMenuItem.Name = "rotateToolStripMenuItem";
+            rotateToolStripMenuItem.Size = new Size(224, 26);
+            rotateToolStripMenuItem.Text = "&Rotate";
+            // 
+            // resizeToolStripMenuItem
+            // 
+            resizeToolStripMenuItem.Name = "resizeToolStripMenuItem";
+            resizeToolStripMenuItem.Size = new Size(224, 26);
+            resizeToolStripMenuItem.Text = "Re&size";
+            // 
+            // propertiesToolStripMenuItem
+            // 
+            propertiesToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { backgroundColorToolStripMenuItem, resizeSettingsToolStripMenuItem });
+            propertiesToolStripMenuItem.Name = "propertiesToolStripMenuItem";
+            propertiesToolStripMenuItem.Size = new Size(90, 24);
+            propertiesToolStripMenuItem.Text = "&Properties";
+            // 
+            // backgroundColorToolStripMenuItem
+            // 
+            backgroundColorToolStripMenuItem.Name = "backgroundColorToolStripMenuItem";
+            backgroundColorToolStripMenuItem.Size = new Size(224, 26);
+            backgroundColorToolStripMenuItem.Text = "&Background Color";
+            // 
+            // resizeSettingsToolStripMenuItem
+            // 
+            resizeSettingsToolStripMenuItem.Name = "resizeSettingsToolStripMenuItem";
+            resizeSettingsToolStripMenuItem.Size = new Size(224, 26);
+            resizeSettingsToolStripMenuItem.Text = "Resize Se&ttings";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            aboutToolStripMenuItem.Size = new Size(64, 24);
+            aboutToolStripMenuItem.Text = "&About";
+            // 
+            // nextImageToolStripMenuItem
+            // 
+            nextImageToolStripMenuItem.Name = "nextImageToolStripMenuItem";
+            nextImageToolStripMenuItem.Size = new Size(224, 26);
+            nextImageToolStripMenuItem.Text = "&Next Image";
+            // 
+            // previousImageToolStripMenuItem
+            // 
+            previousImageToolStripMenuItem.Name = "previousImageToolStripMenuItem";
+            previousImageToolStripMenuItem.Size = new Size(224, 26);
+            previousImageToolStripMenuItem.Text = "Pre&vious Image";
+            // 
             // FormMain
             // 
             AllowDrop = true;
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             AutoScroll = true;
             AutoSize = true;
-            ClientSize = new Size(604, 371);
+            ClientSize = new Size(709, 498);
             Controls.Add(tableLayoutPanel1);
             Controls.Add(pictureBox1);
-            MinimumSize = new Size(620, 50);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Margin = new Padding(3, 4, 3, 4);
+            MinimumSize = new Size(727, 51);
             Name = "FormMain";
             Text = "Image Viewer";
             Load += FormMain_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             tableLayoutPanel1.ResumeLayout(false);
             tableLayoutPanel1.PerformLayout();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -225,5 +355,20 @@
         private Button saveAsButton;
         private SaveFileDialog saveFileDialog1;
         private Button saveButton;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem imageToolStripMenuItem;
+        private ToolStripMenuItem openToolStripMenuItem;
+        private ToolStripMenuItem closeToolStripMenuItem;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private ToolStripMenuItem viewToolStripMenuItem;
+        private ToolStripMenuItem fullScreenToolStripMenuItem;
+        private ToolStripMenuItem rotateToolStripMenuItem;
+        private ToolStripMenuItem resizeToolStripMenuItem;
+        private ToolStripMenuItem propertiesToolStripMenuItem;
+        private ToolStripMenuItem backgroundColorToolStripMenuItem;
+        private ToolStripMenuItem resizeSettingsToolStripMenuItem;
+        private ToolStripMenuItem aboutToolStripMenuItem;
+        private ToolStripMenuItem nextImageToolStripMenuItem;
+        private ToolStripMenuItem previousImageToolStripMenuItem;
     }
 }
